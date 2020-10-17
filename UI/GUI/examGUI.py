8f03ac5cb2ecba29ddfb032a1a7658d5d166d21b
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-10-17 21:37:15 +0300

    Aproximate design of GUI implemented

---
+++ UI/GUI/examGUI.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from tkinter.ttk import Notebook
+
+from UI.GUI.page import Page
+
+class Exam(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+
+       all = Frame(self)
+       all.pack(side=LEFT, fill=Y)
+       all_title = Label(all, text="All exams")
+       all_title.pack()
+       scrollbar = Scrollbar(all)
+       scrollbar.pack(side=RIGHT, fill=Y)
+       languages_listbox = Listbox(all, yscrollcommand=scrollbar.set, width=100)
+       languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
+                    "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
+                    "T-SQL", "PL-SQL", "Typescript"]
+       for language in languages:
+           languages_listbox.insert(END, language)
+       languages_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=languages_listbox.yview)
+
+       #################################################################################################
+
+       find = Frame(self)
+       find.pack(side=LEFT, fill=Y)
+       find_title = Label(find, text="Find exam")
+       find_title.pack()
+       scrollbar = Scrollbar(find)
+       scrollbar.pack(side=RIGHT, fill=Y)
+       languages_listbox = Listbox(find, yscrollcommand=scrollbar.set, width=100)
+       languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
+                    "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
+                    "T-SQL", "PL-SQL", "Typescript"]
+       for language in languages:
+           languages_listbox.insert(END, language)
+       languages_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=languages_listbox.yview)
+
+       ########################################################################################################
+
+       add = Frame(self)
+       add.pack()
+
+       add_title = Label(add, text="Add exam", font="Helvetica -14 bold")
+       add_title.grid()
+
+       surn_title = Label(add, text="Surname: ")
+       surn_title.grid(row=1, column=0)
+       surn = Entry(add, width=50)
+       surn.grid(row=1, column=1)
+
+       name_title = Label(add, text="Name: ")
+       name_title.grid(row=2, column=0)
+       name = Entry(add, width=50)
+       name.grid(row=2, column=1)
+
+       patr_title = Label(add, text="Patronymic: ")
+       patr_title.grid(row=3, column=0)
+       patr = Entry(add, width=50)
+       patr.grid(row=3, column=1)
+
+       addr_title = Label(add, text="Address: ")
+       addr_title.grid(row=4, column=0)
+       addr = Entry(add, width=50)
+       addr.grid(row=4, column=1)
+
+       birth_title = Label(add, text="Date of birthday: ")
+       birth_title.grid(row=5, column=0)
+       birth = Entry(add, width=50)
+       birth.grid(row=5, column=1)
+
+       passp_title = Label(add, text="Number of passport: ")
+       passp_title.grid(row=6, column=0)
+       passp = Entry(add, width=50)
+       passp.grid(row=6, column=1)
+
+       add_btn = Button(add, text="Add enrollee")
+       add_btn.grid()
--- UI/GUI/main_page.py
+++ UI/GUI/main_page.py
@@ -1,14 +1,36 @@
+# import tkinter
+# #
+# # top = tkinter.Tk()
+# # text = tkinter.Label(top, text = "Select type to work with")
+# # enrollee = tkinter.Button(top,text="Enrollee",
+# #                       command=top.quit)
+# # examiner = tkinter.Button(top,text="Examiner",
+# #                       command=top.quit)
+# # exam = tkinter.Button(top,text="Exam",
+# #                       command=top.quit)
+# # text.pack()
+# # enrollee.pack()
+# # examiner.pack()
+# # exam.pack()
+# # tkinter.mainloop()
+
 from tkinter import *
+from tkinter.ttk import Notebook
 
-root = Tk()
+from UI.GUI.enrolleeGUI import Enrollee
+from UI.GUI.examinerGUI import Examiner
+from UI.GUI.examGUI import Exam
 
-def Hello(event):
-    print ("Yet another hello world")
-
-btn = Button(root,                  #родительское окно
-             text="Click me",       #надпись на кнопке
-             width=30,height=5,     #ширина и высота
-             bg="white",fg="black") #цвет фона и надписи
-btn.bind("<Button-1>", Hello)       #при нажатии ЛКМ на кнопку вызывается функция Hello
-btn.pack()                          #расположить кнопку на главном окне
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    root.update()
+    tab_control = Notebook(root)
+    p1 = Enrollee()
+    p2 = Examiner()
+    p3 = Exam()
+    tab_control.add(p1, text = "Enrollee")
+    tab_control.add(p2, text="Examiner")
+    tab_control.add(p3, text="Exam")
+    tab_control.pack(expand=1, fill='both')
+    root.state('zoomed')
+    root.mainloop()
