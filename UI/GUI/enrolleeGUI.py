f7c2278c9e5387b623d21008e8f0db5e2e4c554c
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-10-18 19:32:39 +0300

    Design of main pages implemented

--- UI/GUI/enrolleeGUI.py
+++ UI/GUI/enrolleeGUI.py
@@ -9,73 +9,58 @@ class Enrollee(Page):
 
        all = Frame(self)
        all.pack(side = LEFT, fill = Y)
-       all_title = Label(all, text = "All enrollees")
-       all_title.pack()
+       Label(all, text = "All enrollees").pack()
        scrollbar = Scrollbar(all)
        scrollbar.pack(side=RIGHT, fill=Y)
-       languages_listbox = Listbox(all, yscrollcommand=scrollbar.set,  width=100)
+       all_listbox = Listbox(all, yscrollcommand=scrollbar.set,  width=100)
        languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
                     "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
                     "T-SQL", "PL-SQL", "Typescript"]
        for language in languages:
-           languages_listbox.insert(END, language)
-       languages_listbox.pack(side="top", fill="both", expand=True)
-       scrollbar.config(command=languages_listbox.yview)
+           all_listbox.insert(END, language)
+       all_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=all_listbox.yview)
 
 #################################################################################################
 
        find = Frame(self)
        find.pack(side = LEFT, fill = Y)
-       find_title = Label(find, text = "Find enrollee")
-       find_title.pack()
+       Label(find, text = "Find enrollee").pack()
+       find_entry = Entry(find).pack()
        scrollbar = Scrollbar(find)
        scrollbar.pack(side=RIGHT, fill=Y)
-       languages_listbox = Listbox(find, yscrollcommand=scrollbar.set,  width=100)
+       find_listbox = Listbox(find, yscrollcommand=scrollbar.set,  width=100)
        languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
                     "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
                     "T-SQL", "PL-SQL", "Typescript"]
        for language in languages:
-           languages_listbox.insert(END, language)
-       languages_listbox.pack(side="top", fill="both", expand=True)
-       scrollbar.config(command=languages_listbox.yview)
+           find_listbox.insert(END, language)
+       find_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=find_listbox.yview)
 
 ########################################################################################################
 
        add = Frame(self)
        add.pack()
 
-       add_title = Label(add, text = "Add enrollee", font = "Helvetica -14 bold")
-       add_title.grid()
+       Label(add, text = "Add enrollee", font = "Helvetica -14 bold").grid(columnspan = 2)
 
-       surn_title = Label(add, text = "Surname: ")
-       surn_title.grid(row = 1, column = 0)
-       surn = Entry(add, width = 50)
-       surn.grid(row = 1, column = 1)
+       Label(add, text = "Surname: ").grid(row = 1, column = 0)
+       surn = Entry(add, width = 50).grid(row = 1, column = 1)
 
-       name_title = Label(add, text = "Name: ")
-       name_title.grid(row = 2, column = 0)
-       name = Entry(add, width = 50)
-       name.grid(row = 2, column = 1)
+       Label(add, text = "Name: ").grid(row = 2, column = 0)
+       name = Entry(add, width = 50).grid(row = 2, column = 1)
 
-       patr_title = Label(add, text = "Patronymic: ")
-       patr_title.grid(row = 3, column = 0)
-       patr = Entry(add, width = 50)
-       patr.grid(row = 3, column = 1)
+       Label(add, text = "Patronymic: ").grid(row = 3, column = 0)
+       patr = Entry(add, width = 50).grid(row = 3, column = 1)
 
-       addr_title = Label(add, text = "Address: ")
-       addr_title.grid(row = 4, column = 0)
-       addr = Entry(add, width = 50)
-       addr.grid(row = 4, column = 1)
+       Label(add, text = "Address: ").grid(row = 4, column = 0)
+       addr = Entry(add, width = 50).grid(row = 4, column = 1)
 
-       birth_title = Label(add, text = "Date of birthday: ")
-       birth_title.grid(row = 5, column = 0)
-       birth = Entry(add, width = 50)
-       birth.grid(row = 5, column = 1)
+       Label(add, text = "Date of birthday: ").grid(row = 5, column = 0)
+       birth = Entry(add, width = 50).grid(row = 5, column = 1)
 
-       passp_title = Label(add, text = "Number of passport: ")
-       passp_title.grid(row = 6, column = 0)
-       passp = Entry(add, width = 50)
-       passp.grid(row = 6, column = 1)
+       Label(add, text = "Number of passport: ").grid(row = 6, column = 0)
+       passp = Entry(add, width = 50).grid(row = 6, column = 1)
 
-       add_btn = Button(add, text = "Add enrollee")
-       add_btn.grid()
+       add_btn = Button(add, text = "Add enrollee").grid(columnspan = 2)
--- UI/GUI/examGUI.py
+++ UI/GUI/examGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter.ttk import Notebook
+from tkinter import ttk
+from tkinter.ttk import *
 
 from UI.GUI.page import Page
 
@@ -9,73 +10,75 @@ class Exam(Page):
 
        all = Frame(self)
        all.pack(side=LEFT, fill=Y)
-       all_title = Label(all, text="All exams")
-       all_title.pack()
+       Label(all, text="All exams").pack()
        scrollbar = Scrollbar(all)
        scrollbar.pack(side=RIGHT, fill=Y)
-       languages_listbox = Listbox(all, yscrollcommand=scrollbar.set, width=100)
+       all_listbox = Listbox(all, yscrollcommand=scrollbar.set, width=100)
        languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
                     "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
                     "T-SQL", "PL-SQL", "Typescript"]
        for language in languages:
-           languages_listbox.insert(END, language)
-       languages_listbox.pack(side="top", fill="both", expand=True)
-       scrollbar.config(command=languages_listbox.yview)
+           all_listbox.insert(END, language)
+       all_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=all_listbox.yview)
 
-       #################################################################################################
+#################################################################################################
 
        find = Frame(self)
        find.pack(side=LEFT, fill=Y)
-       find_title = Label(find, text="Find exam")
-       find_title.pack()
+       Label(find, text="Find exam").pack()
        scrollbar = Scrollbar(find)
        scrollbar.pack(side=RIGHT, fill=Y)
-       languages_listbox = Listbox(find, yscrollcommand=scrollbar.set, width=100)
+       find_listbox = Listbox(find, yscrollcommand=scrollbar.set, width=100)
        languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
                     "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
                     "T-SQL", "PL-SQL", "Typescript"]
        for language in languages:
-           languages_listbox.insert(END, language)
-       languages_listbox.pack(side="top", fill="both", expand=True)
-       scrollbar.config(command=languages_listbox.yview)
+           find_listbox.insert(END, language)
+       find_listbox.pack(side="top", fill="both", expand=True)
+       scrollbar.config(command=find_listbox.yview)
 
        ########################################################################################################
 
        add = Frame(self)
        add.pack()
 
-       add_title = Label(add, text="Add exam", font="Helvetica -14 bold")
-       add_title.grid()
+       Label(add, text="Add exam", font="Helvetica -14 bold").grid(columnspan = 3)
 
-       surn_title = Label(add, text="Surname: ")
-       surn_title.grid(row=1, column=0)
-       surn = Entry(add, width=50)
-       surn.grid(row=1, column=1)
+       Label(add, text="Time of pass(DD.MM.YYYY HH:MM): ").grid(row=1, column=0)
+       time = Entry(add, width=50).grid(row=1, column=1, columnspan = 2)
 
-       name_title = Label(add, text="Name: ")
-       name_title.grid(row=2, column=0)
-       name = Entry(add, width=50)
-       name.grid(row=2, column=1)
+       Label(add, text="Name of exam: ").grid(row=2, column=0)
+       name = Entry(add, width=50).grid(row=2, column=1, columnspan = 2)
 
-       patr_title = Label(add, text="Patronymic: ")
-       patr_title.grid(row=3, column=0)
-       patr = Entry(add, width=50)
-       patr.grid(row=3, column=1)
+       Label(add, text="Score: ").grid(row=3, column=0)
+       score = Entry(add, width=50).grid(row=3, column=1, columnspan = 2)
 
-       addr_title = Label(add, text="Address: ")
-       addr_title.grid(row=4, column=0)
-       addr = Entry(add, width=50)
-       addr.grid(row=4, column=1)
+       Label(add, text="Enrollee: ").grid(row=4, column=0)
+       enr = Entry(add, width=50).grid(row=4, column=1)
+       find_enr_btn = Button(add, text = "Find").grid(row = 4, column = 2)
+       scrollbar = Scrollbar(add)
+       scrollbar.grid()
+       enrolee_listbox = Listbox(add, yscrollcommand=scrollbar.set, width=100)
+       languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
+                    "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
+                    "T-SQL", "PL-SQL", "Typescript"]
+       for language in languages:
+           enrolee_listbox.insert(END, language)
+       enrolee_listbox.grid(row = 5, column = 0, columnspan = 3)
+       scrollbar.config(command=enrolee_listbox.yview)
 
-       birth_title = Label(add, text="Date of birthday: ")
-       birth_title.grid(row=5, column=0)
-       birth = Entry(add, width=50)
-       birth.grid(row=5, column=1)
-
-       passp_title = Label(add, text="Number of passport: ")
-       passp_title.grid(row=6, column=0)
-       passp = Entry(add, width=50)
-       passp.grid(row=6, column=1)
-
-       add_btn = Button(add, text="Add enrollee")
-       add_btn.grid()
+       Label(add, text="Examiner: ").grid(row=6, column=0)
+       examiner = Entry(add, width=50).grid(row=6, column=1)
+       find_examiner_btn = Button(add, text = "Find").grid(row = 6, column = 2)
+       scrollbar = Scrollbar(add)
+       scrollbar.grid()
+       examiner_listbox = Listbox(add, yscrollcommand=scrollbar.set, width=100)
+       languages = ["Python", "JavaScript", "C#", "Java", "C/C++", "Swift",
+                    "PHP", "Visual Basic.NET", "F#", "Ruby", "Rust", "R", "Go",
+                    "T-SQL", "PL-SQL", "Typescript"]
+       for language in languages:
+           examiner_listbox.insert(END, language)
+       examiner_listbox.grid(row = 7, column = 0, columnspan = 3)
+       scrollbar.config(command=examiner_listbox.yview)
+       add_btn = Button(add, text="Add exam").grid(columnspan = 3)
